9d3d211982e24643b5ea82d876813198262cc12c
BorisKaryshev <boris.karyshev@gmail.com>
2025-11-16 12:29:34 +0000

    Updated hw4/script

--- hw4/script.sh
+++ hw4/script.sh
@@ -1,32 +1,33 @@
-#!/bin/bash 
+#!/bin/bash
 set -eou pipefail
-set -x 
+set -x
 
 install_dependencies ()
 {
+
     packages=$(
 cat <<EOF
-        build-essential 
-        fakeroot 
-        libncurses-dev 
-        libncurses5-dev 
-        bc 
-        bison 
-        flex 
-        libssl-dev 
-        libelf-dev 
-        kmod 
-        ccache 
-        dwarves 
-        git 
-        rsync 
-        gettext 
-        libnuma-dev 
-        xz-utils 
-        gzip 
-        bzip2 
-        unzip 
-        python3 
+        build-essential
+        fakeroot
+        libncurses-dev
+        libncurses5-dev
+        bc
+        bison
+        flex
+        libssl-dev
+        libelf-dev
+        kmod
+        ccache
+        dwarves
+        git
+        rsync
+        gettext
+        libnuma-dev
+        xz-utils
+        gzip
+        bzip2
+        unzip
+        python3
         python3-dev
 EOF
     )
@@ -46,7 +47,7 @@ cd $working_dir
 
 wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz"
 tar xf "linux-$KERNEL_VERSION.tar.xz" && mv "linux-$KERNEL_VERSION" linux
-cd linux 
+cd linux
 
 yes "" | make oldconfig || true
 scripts/config --disable SYSTEM_TRUSTED_KEYS
